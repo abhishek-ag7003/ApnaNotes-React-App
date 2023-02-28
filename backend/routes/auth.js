f695be347814118e29ab7e87e8812611b0b3c019
Abhishek Gupta <abhishek.ag7003@gmail.com>
2023-02-28 22:28:08 +0530

    manage errors while creating users and validating user data

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,45 @@
-const express = require('express')
-const router = express.Router()
-const User = require("../models/User")
-const { body, validationResult } = require('express-validator');
-
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //create a user using POST "api/auth/user" -- (Doesn't require auth)
-router.post('/user',[
-    body('name','Enter a valid name').isLength({ min: 2 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password','Password must be atleast 5 characters').isLength({ min: 5 }),
-],(req, res)=>{
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 2 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    //If there are any error , it returns Bad Request (400) with error msg
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+
+    // check whether the user with the same email is already exist
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "User with same email id already exist" });
+      }
+      user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-    res.json({error:"Please enter unique mail id", message:err.message})});
+      });
 
-})
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured")
+    }
+  }
+);
 
-module.exports = router
+module.exports = router;
