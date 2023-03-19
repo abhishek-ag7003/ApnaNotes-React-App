55dec1831f29ed6f874a356dd7f660520a8a68e2
Abhishek Gupta <abhishek.ag7003@gmail.com>
2023-03-19 23:02:50 +0530

    Added running server port number

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,22 @@
-const connectToMongo = require("./database_connection")
+const connectToMongo = require("./database_connection");
 
 connectToMongo();
 
-const express = require('express')
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 // app.get("/",(req,res)=>{
 //     res.send("Hello")
 // })
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(cors());
 
 //Available routes
-app.use("/api/auth", require("./routes/auth"))
-app.use("/api/notes", require("./routes/notes"))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
-app.listen(port, ()=>{
-    console.log(`Server is running at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
